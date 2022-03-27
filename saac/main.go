2a9a823573f421cbdc240fa4a01a86366efdf195
pradeepp28 <ppradeep0610@gmail.com>
2022-03-27 14:45:46 -0500

    add stabdalone client to call access server

---
+++ saac/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"github.com/onflow/flow-go-sdk/client"
+
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	accessAddr := ":9000"
+	flag.StringVar(&accessAddr, "access-address", ":9000", "Access insecure gRPC server address")
+	flag.Parse()
+	flowClient, err := client.New(accessAddr, grpc.WithInsecure()) //nolint:staticcheck
+	if err != nil {
+		log.Error().Err(err).Msg("failed to initialize gRPC client")
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := flowClient.Ping(ctx); err != nil {
+		log.Error().Err(err).Msg("failed to ping access node")
+		return
+	}
+}
