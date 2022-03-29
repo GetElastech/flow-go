633cfa80a567700deb1bf972a6acdf805f651eed
pradeepp28 <ppradeep0610@gmail.com>
2022-03-29 17:19:58 -0500

    Add get latest block client

--- saac/main.go
+++ saac/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"time"
 
+	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/client"
 
 	"github.com/rs/zerolog/log"
@@ -13,17 +14,36 @@ import (
 
 func main() {
 	accessAddr := ":9000"
-	flag.StringVar(&accessAddr, "access-address", ":9000", "Access insecure gRPC server address")
+	flag.StringVar(&accessAddr, "access-node-addr", ":9000", "Access insecure gRPC server address")
 	flag.Parse()
+	log.Info().Msgf("Access node: %v", accessAddr)
 	flowClient, err := client.New(accessAddr, grpc.WithInsecure()) //nolint:staticcheck
 	if err != nil {
 		log.Error().Err(err).Msg("failed to initialize gRPC client")
 		return
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	if err := flowClient.Ping(ctx); err != nil {
+	pctx, pcancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer pcancel()
+	if err := flowClient.Ping(pctx); err != nil {
 		log.Error().Err(err).Msg("failed to ping access node")
 		return
 	}
+	sealedBlock, err := getLatestBlock(flowClient, true)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get latest sealed block")
+		return
+	}
+	log.Info().Msgf("Sealed block: %+v", sealedBlock)
+	unsealedBlock, err := getLatestBlock(flowClient, false)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get latest unsealed block")
+		return
+	}
+	log.Info().Msgf("Unsealed block: %+v", unsealedBlock)
+}
+
+func getLatestBlock(flowClient *client.Client, isSealed bool) (*flow.Block, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return flowClient.GetLatestBlock(ctx, true)
 }
