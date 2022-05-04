1dd7c01be4583b580f0fc1c1a536df63cfe7994c
GetElastech <codesubmit@getelastech.com>
2022-05-04 02:21:03 +0000

    [2063] Refactor code within new separate Access Node and Observer builders Author: Miklos Szegedi

---
+++ cmd/observer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	nodebuilder "github.com/onflow/flow-go/cmd/access/node_builder"
+)
+
+func main() {
+	anb := nodebuilder.FlowAccessNode()
+
+	anb.PrintBuildVersionDetails()
+
+	// parse all the command line args
+	if err := anb.ParseFlags(); err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+
+	// choose a staked or an unstaked node builder based on anb.staked
+	var builder nodebuilder.AccessNodeBuilder
+	builder = nodebuilder.NewUnstakedAccessNodeBuilder(anb)
+
+	if err := builder.Initialize(); err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+
+	node, err := builder.Build()
+	if err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+	node.Run()
+}
+
