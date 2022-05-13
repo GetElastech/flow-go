f1e80fe700e4b6cda8fb0dab21574377979fa795
Pradeep Palanisamy <pradeep.palanisamy@toyota.com>
2022-05-13 11:37:08 -0500

    Fix go imports

--- engine/protocol/api.go
+++ engine/protocol/api.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"errors"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/flow-go/state/protocol"
 	"github.com/onflow/flow-go/storage"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type API interface {
