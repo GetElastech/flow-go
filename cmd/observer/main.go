67085af9e5fc42d100de2b1430faf321cd37a704
GetElastech <codesubmit@getelastech.com>
2022-06-03 03:53:57 +0000

    [2066] Setup CI pipeline and tools to build Observer binary and docker containers Author: Miklos Szegedi, Pradeep Palanisamy

---
+++ cmd/observer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	nodebuilder "github.com/onflow/flow-go/cmd/observer/node_builder"
+)
+
+func main() {
+	anb := nodebuilder.NewFlowObserverServiceBuilder()
+
+	anb.PrintBuildVersionDetails()
+
+	// parse all the command line args
+	if err := anb.ParseFlags(); err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+
+	builder := nodebuilder.NewObserverServiceBuilder(anb)
+
+	if err := builder.Initialize(); err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+
+	node, err := builder.Build()
+	if err != nil {
+		anb.Logger.Fatal().Err(err).Send()
+	}
+	node.Run()
+}
